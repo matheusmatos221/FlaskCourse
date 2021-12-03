4d191fa8c83f9341229e469ec1cfd3265d587462
Matheus Matos <matheus.matos@accurate.com.br>
2021-12-03 18:48:05 -0300

    3.Criação de um novo Jogos

--- learning/Curso_Flask/jogoteca/jogoteca.py
+++ learning/Curso_Flask/jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,12 +8,26 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+lista = [jogo1, jogo2]
 
-@app.route("/inicio")
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    lista = [jogo1, jogo2]
+@app.route("/")
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run(debug=True)
