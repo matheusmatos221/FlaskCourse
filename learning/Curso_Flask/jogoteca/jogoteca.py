a257b214c71dcc347840643171ce7ceb31edc4b3
Matheus Matos <matheus.matos@accurate.com.br>
2021-12-06 23:40:35 -0300

    Redireciona para a proxima pagina mesmo se errar a senha

--- learning/Curso_Flask/jogoteca/jogoteca.py
+++ learning/Curso_Flask/jogoteca/jogoteca.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session, flash, url
 app = Flask(__name__)
 app.secret_key = 'matheus'
 
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.__nome = nome
@@ -21,9 +22,10 @@ class Jogo:
     def console(self):
         return self.__console
 
+
 class Usuario:
-    def __init__(self, id, nome, senha):
-        self.__id = id
+    def __init__(self, user_id, nome, senha):
+        self.__id = user_id
         self.__nome = nome
         self.__senha = senha
 
@@ -39,6 +41,7 @@ class Usuario:
     def senha(self):
         return self.__senha
 
+
 usuario1 = Usuario('mazzatheus', 'Matheus Matos', '12321')
 usuario2 = Usuario('azzam221', 'Azzam Zao', 'azzamzam')
 usuario3 = Usuario('math211', 'Mathias Camargo', 'mac1')
@@ -52,16 +55,21 @@ jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
 jogo3 = Jogo('GTA San Andreas', 'RPG', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
+
 @app.route("/")
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
+
 @app.route('/novo')
 def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        # Usuário não está logado
         return redirect(url_for('login', proxima=url_for('novo')))
     else:
+        # Usuário está logado
         return render_template('novo.html', titulo='Novo Jogo')
+
 
 @app.route('/criar', methods=['POST', ])
 def criar():
@@ -72,30 +80,36 @@ def criar():
     lista.append(jogo)
     return redirect(url_for('index'))
 
+
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
     return render_template('login.html', proxima=proxima)
 
+
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
+    """ Valida se o id e senha do usuário estão cadastrados"""
+    proxima_pagina = request.form['proxima']  # Se houver valor, atribui à variável
     if request.form['usuario'] in usuarios:
         usuario = usuarios[request.form['usuario']]
         if request.form['senha'] == usuario.senha:
+            # Usuário Válido
             session['usuario_logado'] = usuario.id
             flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
             return redirect(proxima_pagina)
+    # Usuário Inválido
     flash('ID ou Senha inválido!')
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
+    if proxima_pagina:
+        return redirect(url_for('login', proxima=proxima_pagina))
+    return redirect(url_for('login'))
+
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
     return redirect(url_for('index'))
-
 
 
 app.run(debug=True)
