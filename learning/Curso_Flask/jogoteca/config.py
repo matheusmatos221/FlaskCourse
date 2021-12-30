e0967c19013c528225e2cd65f056b8ac6e22c341
Matheus da Cruz Matos <45506460+matheusmatos221@users.noreply.github.com>
2021-12-29 22:06:15 -0300

    Update config.py

--- learning/Curso_Flask/jogoteca/config.py
+++ learning/Curso_Flask/jogoteca/config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = 'matheus'
 MYSQL_HOST = "127.0.0.1"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "Mazzatheus1!"
+MYSQL_PASSWORD = "Senha123"
 MYSQL_DB = "jogoteca"
 MYSQL_PORT = 3306
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
